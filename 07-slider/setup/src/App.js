0cd10cec219da631829ac85801563b59d72b2aad
FatimahFarooq <fatimafarooq47@gmai.com>
2022-03-28 00:30:33 +0300

    slider
    slider implemented

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,9 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
-  return <h2>slider project setup</h2>;
+  const [index, setIndex] = useState(0);
+  const [review, setReview] = useState(data[index]);
+
+  setTimeout(() => {
+    setIndex(index === data.length - 1 ? 0 : index + 1);
+    setReview(data[index]);
+  }, 5000);
+
+  const prevReview = () => {
+    console.log(index);
+    setIndex(index === 0 ? data.length - 1 : index - 1);
+    setReview(data[index]);
+    console.log(review);
+  };
+
+  const nextReview = () => {
+    setIndex(index === data.length - 1 ? 0 : index + 1);
+    setReview(data[index]);
+  };
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        <article>
+          <img src={review.image} alt={review.name} className="person-img" />
+          <h4>{review.name}</h4>
+          <p className="title">{review.title}</p>
+          <p className="text">{review.quote}</p>
+          <FaQuoteRight className="icon" />
+        </article>
+        <button className="prev" onClick={prevReview}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={nextReview}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
