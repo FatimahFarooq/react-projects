754616ae5edea64d5b93d11466b27cb9d752579c
FatimahFarooq <fatimafarooq47@gmai.com>
2022-03-27 23:45:02 +0300

    Questions/Accordion
    accordion implmented

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ question }) => {
+  const [isShow, setIsShow] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  return (
+    <article className="question" key={question.id}>
+      <header>
+        <h4>{question.title} </h4>
+        <button
+          className="btn"
+          onClick={() => {
+            setIsShow(!isShow);
+            setIndex(question.id);
+          }}
+        >
+          {isShow && question.id === index ? (
+            <AiOutlineMinus />
+          ) : (
+            <AiOutlinePlus />
+          )}
+        </button>
+      </header>
+      <p>{isShow && question.id === index && question.info}</p>
+    </article>
+  );
 };
 
 export default Question;
